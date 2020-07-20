aa183a1601d9090b3b86afd909bf1049bc812392
IT-shendaojun <497902218@qq.com>
2020-07-20 18:30:31 +0800

    demo更新

--- app/src/main/java/com/avit/safe/demo/MainActivity.java
+++ app/src/main/java/com/avit/safe/demo/MainActivity.java
@@ -8,6 +8,8 @@ import com.avit.safe.sqlite.AsyncDatabaseOperator;
 import com.avit.safe.sqlite.IDatabaseOperation;
 import com.avit.safe.sqlite.SafeDatabaseOperator;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,7 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
         IDatabaseOperation<TestBean> normal = new Normal();
         IDatabaseOperation<TestBean> safe = SafeDatabaseOperator.safe(normal);
-        IDatabaseOperation<TestBean> async = AsyncDatabaseOperator.async(safe);
+        safe.read();
+
+
+        AsyncDatabaseOperator<TestBean> async = AsyncDatabaseOperator.async(safe);
+        async.onListener(new AsyncDatabaseOperator.AsyncListener() {
+            @Override
+            public void onDone(int count, Object src) {
+
+            }
+        }).count();
+
+        async.onReadListener(new AsyncDatabaseOperator.AsyncReadListener() {
+            @Override
+            public void onRead(int count, Object object, List rets) {
+
+            }
+
+            @Override
+            public void onDone(int count, Object src) {
+
+            }
+        }).read();
 
     }
 }
